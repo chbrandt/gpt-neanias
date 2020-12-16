f1f3b0fbd0ba4663d5acb9a679df0ac881d0fd8a
Carlos H Brandt <c.brandt@jacobs-university.de>
2020-12-16 18:24:53 +0100

    Make links from gpt/utils to utils here

--- npt/utils/geojson.py
+++ npt/utils/geojson.py
@@ -1,14 +1,2 @@
+from gpt.utils.geojson import read
 from .formatters import geojson_2_geodataframe, products_2_geojson, json_2_geojson
-
-import json
-
-from . import log
-
-def read(filename):
-    """
-    Return 'features' from GeoJSON
-    """
-    with open(filename, 'r') as fp:
-        js = json.load(fp)
-        features = js['features']
-    return features

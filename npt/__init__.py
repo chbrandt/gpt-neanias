a502822e7e1736c665a2b4a4e53b8d763ef2ee81
Carlos H Brandt <c.brandt@jacobs-university.de>
2021-03-04 17:58:06 +0100

    Reimplement log interface

--- npt/__init__.py
+++ npt/__init__.py
@@ -5,5 +5,5 @@ This tools are meant to support the data handling of Planetary group
 of the H2024 NEANIAS project.
 You'll find here tools to search, download and process Planetary data.
 """
-from ._log import log
+from . import _log as log
 # from ._db import db
